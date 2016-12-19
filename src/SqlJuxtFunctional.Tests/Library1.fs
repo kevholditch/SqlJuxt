d8bdc8183ffc50e2e17086836e0173c2846f65d7
Kevin Holditch <kevholditch@hotmail.com>
2016-12-19 14:05:00 +0000

    added failing database builder test in f sharp

---
+++ src/SqlJuxtFunctional.Tests/Library1.fs
@@ -0,0 +1,24 @@
+﻿namespace SqlJuxtFunctionalTests
+
+
+open FsUnit
+open SqlJuxtFunctional.DatabaseBuilder.TableBuilder
+
+    module TablebuilderTests =
+        open Xunit
+
+        [<Fact>]
+        let ``should be able to builder a table with a single int column``() =
+            Create "MyTable"
+                |> WithNullableInt "TestTable"
+                |> Build 
+                |> should equal @"CREATE TABLE [dbo].[TestTable](
+[Column1] [int] NULL
+)
+GO
+
+"
+
+
+
+
